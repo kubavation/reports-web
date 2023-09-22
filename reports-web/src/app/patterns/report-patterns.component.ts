d84761e647575b0be8001642b98345e2efb652c9
kubavation <37585627+kubavation@users.noreply.github.com>
2023-09-22 18:34:19 +0200

    modules.service - mock

--- reports-web/src/app/patterns/report-patterns.component.ts
+++ reports-web/src/app/patterns/report-patterns.component.ts
@@ -1,7 +1,8 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {BehaviorSubject, filter, switchMap} from "rxjs";
-import {Subsystem} from "../shared/model/subsystem";
+import {BehaviorSubject, filter, Subject, switchMap, tap} from "rxjs";
+import {Subsystem} from "../shared/modules/model/subsystem";
 import {ReportPatternsService} from "./service/report-patterns.service";
+import {ModulesService} from "../shared/modules/service/modules.service";
 
 @Component({
   selector: 'app-report-patterns',
@@ -13,12 +14,18 @@ export class ReportPatternsComponent {
 
   private subsystemSubject = new BehaviorSubject<Subsystem>(null);
 
+  modules$ = this.modulesService.modules$
+    .pipe(
+      tap(modules => this.subsystemSubject.next(modules[0]))
+    );
+
   reportPatterns$ = this.subsystemSubject.pipe(
     filter(subsystem => !!subsystem),
     switchMap(({name}) => this.reportPatternsService.reportPatterns(name))
   )
 
-  constructor(private reportPatternsService: ReportPatternsService) {
+  constructor(private reportPatternsService: ReportPatternsService,
+              private modulesService: ModulesService) {
   }
 
 }

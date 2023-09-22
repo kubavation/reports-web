41eb1c79f2802b7d2bf248b9e8faab86047a5656
kubavation <37585627+kubavation@users.noreply.github.com>
2023-09-22 19:12:01 +0200

    fix styling

--- reports-web/src/app/shared/shared.module.ts
+++ reports-web/src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HttpClientModule} from "@angular/common/http";
+import {MatTableModule} from "@angular/material/table";
 
 
 
@@ -8,7 +9,11 @@ import {HttpClientModule} from "@angular/common/http";
   declarations: [],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    MatTableModule
+  ],
+  exports: [
+    MatTableModule
   ]
 })
 export class SharedModule { }

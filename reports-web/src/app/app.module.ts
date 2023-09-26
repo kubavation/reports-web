b86f05c2e9c6611743311c290851a8c7621470b9
kubavation <37585627+kubavation@users.noreply.github.com>
2023-09-26 17:35:06 +0200

    http loading interceptor

--- reports-web/src/app/app.module.ts
+++ reports-web/src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {initializeKeycloak} from "./keycloak/init/keycloak-init.factory";
 import {KeycloakAngularModule, KeycloakService} from "keycloak-angular";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {SharedModule} from "./shared/shared.module";
 import {NgxExtendedPdfViewerModule} from "ngx-extended-pdf-viewer";
+import {LoadingHttpInterceptor} from "./common/interceptors/loading-http.interceptor";
 
 @NgModule({
   declarations: [
@@ -28,6 +29,10 @@ import {NgxExtendedPdfViewerModule} from "ngx-extended-pdf-viewer";
       useFactory: initializeKeycloak,
       multi: true,
       deps: [KeycloakService],
+    },
+    { provide: HTTP_INTERCEPTORS,
+      useClass: LoadingHttpInterceptor,
+      multi: true
     }
   ],
   bootstrap: [AppComponent]

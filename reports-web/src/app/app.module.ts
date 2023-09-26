1e6d3e7e0ed69f0865c7c586faf87cab699b651a
kubavation <37585627+kubavation@users.noreply.github.com>
2023-09-26 17:47:05 +0200

    loader component

--- reports-web/src/app/app.module.ts
+++ reports-web/src/app/app.module.ts
@@ -10,10 +10,12 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {SharedModule} from "./shared/shared.module";
 import {NgxExtendedPdfViewerModule} from "ngx-extended-pdf-viewer";
 import {LoadingHttpInterceptor} from "./common/interceptors/loading-http.interceptor";
+import { LoaderComponent } from './common/loader/loader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoaderComponent
   ],
     imports: [
         BrowserModule,
